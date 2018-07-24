e24fbaa80d5b09e134b3fa8b185770a407bc67fd
mprzepiorkowski <mprzepiorkowski@gmail.com>
2018-07-24 09:26:48 +0200

    added project files

---
+++ eda.py
@@ -0,0 +1,74 @@
+import numpy as np
+from sklearn import linear_model
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeRegressor
+
+from preprocess import load_data_set, prepare_data_set, write_csv
+
+
+def main():
+    file = 'train.csv'
+
+    try:
+        data, header = prepare_data_set(load_data_set(file))
+        data = data.astype(np.float)
+
+        sliced_data = data[:, :data.shape[1] - 1]
+        target = data[:, data.shape[1] - 1:].astype(np.float)
+
+        print('target min: ', np.min(target))
+        print('target max: ', np.max(target))
+        print('target median: ', np.median(target))
+        print()
+
+        np.set_printoptions(precision=3, linewidth=140, suppress=True)
+
+        print()
+        print('regression: ')
+
+        models = {
+            # 'linear_regression': linear_model.LinearRegression(),
+            # 'linear_ridge': linear_model.Ridge(),
+            # 'lasso': linear_model.Lasso(),
+            # 'elastic_net': linear_model.ElasticNet(),
+            # 'bayesian_ridge': linear_model.BayesianRidge(),
+            # 'decision_tree': DecisionTreeRegressor(max_depth=5),
+            'random_forest': RandomForestRegressor()
+        }
+
+        for name, model in models.items():
+            print('model: ', name)
+
+            model.fit(sliced_data, target.ravel())
+            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='neg_mean_absolute_error', cv=10)
+            print('mean: ', scores.mean())
+
+            model.fit(sliced_data, target.ravel())
+            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='neg_mean_absolute_error', cv=10)
+            print('neg mean absolute error: ', scores.mean())
+
+            model.fit(sliced_data, target.ravel())
+            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='neg_mean_squared_error', cv=10)
+            print('neg mean squared error: ', scores.mean())
+
+            model.fit(sliced_data, target.ravel())
+            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='neg_median_absolute_error', cv=10)
+            print('neg median absolute error', scores.mean())
+
+            model.fit(sliced_data, target.ravel())
+            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='r2', cv=10)
+            print('r2', scores.mean())
+
+            predicted = model.predict(sliced_data)
+            output = np.c_[data, predicted]
+            header.append('predict')
+
+            write_csv('output.csv', output, header)
+
+            print()
+    except FileNotFoundError:
+        print('cannot open file for reading: ', file)
+
+
+main()
---
+++ preprocess.py
@@ -0,0 +1,129 @@
+import collections
+import csv
+import datetime
+
+import numpy as np
+
+SCHEMA_DESC = collections.OrderedDict([
+    ('datetime', str),
+    ('season', float),
+    ('holiday', float),
+    ('workingday', float),
+    ('weather', float),
+    ('temp', float),
+    ('atemp', float),
+    ('humidity', float),
+    ('windspeed', float),
+    ('casual', float),
+    ('registered', float),
+    ('count', float)
+])
+
+
+def process_data_row(row: list, schema: list) -> list:
+    processed = []
+
+    if len(row) != len(schema):
+        return processed
+
+    for idx, value in enumerate(row):
+        if schema[idx]:
+            processed.append(schema[idx](row[idx]))
+
+    return processed
+
+
+def read_csv(file: str) -> np.array:
+    with open(file) as csv_file:
+        reader = csv.reader(csv_file)
+        data = []
+
+        for idx, row in enumerate(reader):
+            if not idx:
+                continue
+
+            data.append(process_data_row(row, [SCHEMA_DESC[c] for c in SCHEMA_DESC]))
+
+    return np.array(data)
+
+
+def write_csv(file: str, data: np.array, header: list) -> None:
+    with open(file, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        if header:
+            writer.writerow(header)
+
+        writer.writerows(data)
+
+
+def remove_count_outliers(data: np.array, std_margin: float):
+    return data[np.abs(data[:, 11].astype(np.float) - data[:, 11].astype(np.float).mean())
+                <= std_margin * data[:, 11].astype(np.float).std()]
+
+
+def extract_date_time(data: np.array, schema: dict) -> (np.array, list):
+    extracted_dates = []
+
+    for r in data:
+        parsed_date = datetime.datetime.strptime(r[0], "%Y-%m-%d %H:%M:%S")
+        date = [parsed_date.month, parsed_date.weekday(), parsed_date.hour]
+        extracted_dates.append(date)
+
+    header = ['month', 'week_day', 'hour']
+    header += [v for v in schema.keys() if v != 'datetime']
+
+    return np.c_[np.array(extracted_dates), data[:, 1:]], header
+
+
+def remove_column(data: np.array, index: int, header: list):
+    data = np.delete(data, index, 1)
+    del header[index]
+
+    return data, header
+
+
+def load_data_set(file: str) -> np.array:
+    return read_csv(file)
+
+
+def round_numbers(data, columnNo):
+    for r in data:
+        r[columnNo] = round(float(r[columnNo]))
+    return data
+
+
+def remove_humidity(data, humi):
+    return data[np.abs(data[:, 9].astype(np.float)) != humi]
+
+
+def remove_weather(data, weather):
+    return data[np.abs(data[:, 6].astype(np.float)) != weather]
+
+
+def prepare_data_set(data: np.array) -> (np.array, list):
+    data_processed, header = extract_date_time(data, SCHEMA_DESC)
+
+    data_processed = round_numbers(data_processed, 7)
+    data_processed = round_numbers(data_processed, 8)
+    data_processed = round_numbers(data_processed, 9)
+    data_processed = round_numbers(data_processed, 10)
+
+    data_processed, header = remove_column(data_processed, 12, header)
+    data_processed, header = remove_column(data_processed, 11, header)
+
+    data_processed = remove_count_outliers(data_processed, 2.5)
+
+    data_processed = remove_humidity(data_processed, 0)
+    data_processed = remove_weather(data_processed, 4)
+
+    return remove_column(data_processed, 8, header)
+
+
+def main():
+    data = load_data_set('train.csv')
+    processed, header = prepare_data_set(data)
+
+    write_csv('train_processed.csv', processed, header)
+
+
+main()
