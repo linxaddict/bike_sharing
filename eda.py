61b91d801adca4a2fb8b4fd29223c70057d4b890
DESKTOP-SALIV1H\grego <kamilgregorczyk85@gmail.com>
2018-07-24 10:31:20 +0200

    Divide dataset 80%

--- eda.py
+++ eda.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeRegressor
 
-from preprocess import load_data_set, prepare_data_set, write_csv
+from preprocess import load_data_set, prepare_data_set, write_csv, divide_dataset
 
 
 def main():
@@ -16,6 +16,8 @@ def main():
 
         sliced_data = data[:, :data.shape[1] - 1]
         target = data[:, data.shape[1] - 1:].astype(np.float)
+
+        data_train, data_test, target_train, target_test = divide_dataset(sliced_data, target)
 
         print('target min: ', np.min(target))
         print('target max: ', np.max(target))
@@ -40,28 +42,30 @@ def main():
         for name, model in models.items():
             print('model: ', name)
 
-            model.fit(sliced_data, target.ravel())
-            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='neg_mean_absolute_error', cv=10)
+            model.fit(data_train, target_train.ravel())
+            scores = cross_val_score(model, data_train, target_train.ravel(), scoring='neg_mean_absolute_error', cv=10)
             print('mean: ', scores.mean())
 
-            model.fit(sliced_data, target.ravel())
-            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='neg_mean_absolute_error', cv=10)
+            model.fit(data_train, target_train.ravel())
+            scores = cross_val_score(model, data_train, target_train.ravel(), scoring='neg_mean_absolute_error', cv=10)
             print('neg mean absolute error: ', scores.mean())
 
-            model.fit(sliced_data, target.ravel())
-            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='neg_mean_squared_error', cv=10)
+            model.fit(data_train, target_train.ravel())
+            scores = cross_val_score(model, data_train, target_train.ravel(), scoring='neg_mean_squared_error', cv=10)
             print('neg mean squared error: ', scores.mean())
 
-            model.fit(sliced_data, target.ravel())
-            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='neg_median_absolute_error', cv=10)
+            model.fit(data_train, target_train.ravel())
+            scores = cross_val_score(model, data_train, target_train.ravel(), scoring='neg_median_absolute_error',
+                                     cv=10)
             print('neg median absolute error', scores.mean())
 
-            model.fit(sliced_data, target.ravel())
-            scores = cross_val_score(model, sliced_data, target.ravel(), scoring='r2', cv=10)
+            model.fit(data_train, target_train.ravel())
+            scores = cross_val_score(model, data_train, target_train.ravel(), scoring='r2', cv=10)
             print('r2', scores.mean())
 
-            predicted = model.predict(sliced_data)
-            output = np.c_[data, predicted]
+            predicted = model.predict(data_test)
+            output = np.c_[target_test, predicted]
+
             header.append('predict')
 
             write_csv('output.csv', output, header)
